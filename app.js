1a4a4304a4dc3fb3c4873c35848b530ab9616f52
Guillermo R <79545062+NewStudent53@users.noreply.github.com>
2023-05-19 19:33:56 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function domainName (){
+    let a=0;
+    let b=0;
+    let c=0;
+
+    let pronoun = ['the', 'our'];
+    let adj = ['great','big'];
+    let noun = ['jogger','racoon'];
+    
+    let Domain = "";
+    
+    for (let a=0;a<pronoun.length;a++)
+    {
+        for (let b=0;b<adj.length;b++)
+        {
+            for (let c=0;c<noun.length;c++)
+            {
+                Domain += pronoun[a]+adj[b]+noun[c]+".com <br>";
+            }
+        }
+    }
+    return Domain;
+}
+
+document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#name").innerHTML = domainName();
+});
